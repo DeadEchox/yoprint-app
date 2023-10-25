da19b363259990b929a963cb854b366c8ccaeecc
JackDay <jack@sellstream.io>
2023-10-25 14:28:07 +0800

    removed template wording

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -86,10 +86,10 @@
     <footer class="main-footer">
         <!-- To the right -->
         <div class="float-right d-none d-sm-inline">
-            Anything you want
+            YoPrint Assessment.
         </div>
         <!-- Default to the left -->
-        <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+        <strong>YoPrint Assessment.</strong>
     </footer>
 </div>
 <!-- ./wrapper -->
